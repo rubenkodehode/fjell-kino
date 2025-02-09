0798fcffbc04c25b51d4a0942981489b117a61a6
Cibzo <ruben1.kodehode@gmail.com>
2025-02-09 18:45:13 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
-import './index.css';
+import '"./styles/App.module.css";'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
