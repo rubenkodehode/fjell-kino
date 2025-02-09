c8bbc9f47d7289b6305db96d9c0b3033accf38b6
Cibzo <ruben1.kodehode@gmail.com>
2025-02-09 19:10:58 +0100

    god I hope this work

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,28 @@ import "./styles/App.module.css";
 
 function App() {
   return (
-    <AccessibilityProvider>
-      <ThemeProvider>
-        <Router>
-          <div id="root">
-            <Header />
-            <div className="mainContent">
-              <Routes>
-                <Route path="/" element={<Movies />} />
-                <Route path="/movies" element={<Movies />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/booking" element={<Booking />} />
-                <Route path="/upcomingMovies" element={<UpcomingMovies />} />
-                <Route path="/contact" element={<Contact />} />
-              </Routes>
+    <div className={styles.appContainer}>
+      <AccessibilityProvider>
+        <ThemeProvider>
+          <Router>
+            <div id="root">
+              <Header />
+              <div className="mainContent">
+                <Routes>
+                  <Route path="/" element={<Movies />} />
+                  <Route path="/movies" element={<Movies />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/booking" element={<Booking />} />
+                  <Route path="/upcomingMovies" element={<UpcomingMovies />} />
+                  <Route path="/contact" element={<Contact />} />
+                </Routes>
+              </div>
+              <Footer />
             </div>
-            <Footer />
-          </div>
-        </Router>
-      </ThemeProvider>
-    </AccessibilityProvider>
+          </Router>
+        </ThemeProvider>
+      </AccessibilityProvider>
+    </div>
   );
 }
 
